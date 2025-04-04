627ab03e5b0b487f8479f7e19414c2cf11e68376
Hima9791 <ebrahim2020559@gmail.com>
2025-04-04 21:45:42 +0200

    Update detailed_pipeline.py
    updated on line
    row_results["Absolute Unit"] = resolve_compound_unit(replace_numbers_keep_sign(val_str), base_units, multipliers_dict)

--- detailed_pipeline.py
+++ detailed_pipeline.py
@@ -119,7 +119,9 @@ def detailed_analysis_pipeline(df, base_units, multipliers_dict):
             # row_results['Normalized Unit (Parens Intact)'] = replace_numbers_keep_sign_outside_parens(val_str)
 
             # 3. Absolute Unit Resolution (Resolve to base units in structure)
-            row_results["Absolute Unit"] = resolve_compound_unit(val_str, base_units, multipliers_dict)
+            #just updated one coulmn
+            row_results["Absolute Unit"] = resolve_compound_unit(replace_numbers_keep_sign(val_str), base_units, multipliers_dict)
+
 
             # 4. Unit Analysis (Main vs Condition, consistency)
             unit_analysis_results = analyze_value_units(val_str, base_units, multipliers_dict)
